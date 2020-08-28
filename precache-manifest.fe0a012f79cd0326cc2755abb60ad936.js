6c1fcc02e0fb4e74a4a31f39bfb4ff1461790222
Mleblanc94 <leblancmichael94@gmail.com>
2020-08-27 22:02:02 -0400

    Updates

---
+++ precache-manifest.fe0a012f79cd0326cc2755abb60ad936.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e84c6f658742902cd7e71919a226f686",
+    "url": "/basketballplayers/index.html"
+  },
+  {
+    "revision": "689abe5c26c3cc8fb9f5",
+    "url": "/basketballplayers/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "2d615ba4e35328291b8c",
+    "url": "/basketballplayers/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "689abe5c26c3cc8fb9f5",
+    "url": "/basketballplayers/static/js/2.391fda0f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/basketballplayers/static/js/2.391fda0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d615ba4e35328291b8c",
+    "url": "/basketballplayers/static/js/main.a9eb01f6.chunk.js"
+  },
+  {
+    "revision": "ffe6fa956891f5ab74b6",
+    "url": "/basketballplayers/static/js/runtime-main.0db7d578.js"
+  }
+]);
