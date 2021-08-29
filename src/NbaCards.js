90d7b1e7b268c15f8aaa35f93788999c8c35beb7
mleblanc94 <leblancmichael94@gmail.com>
2021-08-29 14:15:14 -0400

    made slight changes to player card

--- src/NbaCards.js
+++ src/NbaCards.js
@@ -5,7 +5,7 @@ const NbaCards = ({ teamlogo, picture, name, position, age, height, weight  }) =
      <div className='tc grow bg-light-green br3 pa3 ma2 dib bw2 shadow-5'>
      <img alt='Team Logo' src={teamlogo} height={170} width={170}/>
      <div>
-     <h3>Name: {name}</h3>
+     <h3>{name}</h3>
      <img alt='player image' src={picture} height={180} width={180} />
      <h3>Position: {position}</h3>
      <h3>Age: {age}</h3>
