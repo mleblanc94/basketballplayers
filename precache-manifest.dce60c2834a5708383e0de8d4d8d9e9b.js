256522bcd05bbda6799b6ca30957cc3cac432127
mleblanc94 <leblancmichael94@gmail.com>
2021-08-31 19:52:06 -0400

    Updates

--- precache-manifest.dce60c2834a5708383e0de8d4d8d9e9b.js
+++ precache-manifest.dce60c2834a5708383e0de8d4d8d9e9b.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "31024a1c97dd063c7072d582f38a8082",
+    "revision": "6d4d95fcb991ee06ab6b9cc055afc151",
     "url": "/basketballplayers/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/basketballplayers/static/css/2.3c03f26f.chunk.css"
   },
   {
-    "revision": "68c0c465004288a4adb4",
+    "revision": "a45e76a1b96a0547f6c4",
     "url": "/basketballplayers/static/css/main.5f361e03.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/basketballplayers/static/js/2.391fda0f.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "68c0c465004288a4adb4",
-    "url": "/basketballplayers/static/js/main.abd6dbf1.chunk.js"
+    "revision": "a45e76a1b96a0547f6c4",
+    "url": "/basketballplayers/static/js/main.9e5077c2.chunk.js"
   },
   {
     "revision": "ffe6fa956891f5ab74b6",
